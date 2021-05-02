e8b07dec9f8e665a153411476567997b6585305b
Maksim Sapunov <Jenny6199@yandex.ru>
2021-05-02 13:47:20 +0300

    Complite_task_4

--- task_4.sql
+++ task_4.sql
@@ -1,24 +1,47 @@
--- ЗАДЧАЧА № 4 Подсчитать количество лайков которые получили 10 самых молодых пользователей.
- 
-DESC users;
-DESC profiles;
+-- ЗАДАЧА № 4 Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
+   
 DESC likes;
--- Найдем 10 самых молодых пользователей.
+DESC profiles;
+
+-- Выбираем user_id самых молодых пользователей
 SELECT 
-	user_id 
-		FROM profiles 
-		ORDER BY profiles.birthday 
-		DESC
-		LIMIT 10;
+	profiles.user_id 
+	FROM profiles 
+	ORDER BY birthday 
+	DESC LIMIT 10;
 
--- Посчитаем количство лайков для конкретного id
+-- Считаем количество лайков для конкретного пользователя
 SELECT 
-	*
-	FROM likes
-	WHERE user_id = 15;
-
--- Скомпонуем запросы
-
-
+	COUNT(*) 
+	FROM likes 
+	WHERE 
+		target_type = 'users' AND 
+		target_id = 17;
 	
-
+-- Создаем вложенный запрос получаем столбец с самыми молодыми пользователями и количеством лайков.
+SELECT 
+	user_id, 
+	(SELECT 
+		COUNT(*) 
+		FROM likes 
+		WHERE 
+			target_type = 'users' AND 
+			target_id = profiles.user_id) AS count_of_likes
+		FROM profiles
+		ORDER BY profiles.birthday 
+		DESC LIMIT 10;
+	
+-- Второй уровень вложенности для вычисления общей суммы лайков.
+SELECT 
+	SUM(count_of_likes)
+	FROM 
+		(SELECT 
+			(SELECT 
+				COUNT(*) 
+				FROM likes 
+				WHERE 
+					target_type = 'users' AND 
+					target_id = profiles.user_id) AS count_of_likes
+				FROM profiles
+				ORDER BY profiles.birthday 
+				DESC LIMIT 100) AS likes_of_user;
