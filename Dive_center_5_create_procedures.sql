1411aed6f9d42799cf6c102817a93cac0ad71401
Maksim Sapunov <Jenny6199@yandex.ru>
2021-05-21 13:50:40 +0300

    Update Dive_center_5_create_procedures.sql

--- Dive_center_5_create_procedures.sql
+++ Dive_center_5_create_procedures.sql
@@ -7,7 +7,7 @@ DELIMITER //
 
 -- ПРОЦЕДУРА №1. Персональный лог-бук. В качестве переменной используется id
 -- члена клуба. 
-CREATE PROCEDURE personal_log_book(IN m_id INT)
+CREATE PROCEDURE IF NOT EXISTS personal_log_book(IN m_id INT)
 BEGIN
 	SET @i = m_id;
 	SELECT 
@@ -23,6 +23,20 @@ BEGIN
 		dive_time
 	FROM log_book
 	WHERE user_id = @i OR partner_id = @i;
+END//
+
+
+-- ПРОЦЕДУРА №2. История поездок члена клуба.
+CREATE PROCEDURE IF NOT EXISTS personal_travel_log (IN m_id INT)
+BEGIN
+	SET @i = m_id;
+	SELECT 
+		(SELECT CONCAT(m.last_name, ' ', m.first_name) 
+			FROM members AS m WHERE tl.user_id = m.id) AS 'Member',
+		travel_date,
+		country
+	FROM travel_log AS tl
+	WHERE user_id = @i;
 END//
 
 DELIMITER ;
